7480d0950ddb314559a6d74bdaa41e89b7856ed5
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-04-11 18:41:18 +0500

    correct_user method created

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -58,6 +58,10 @@ class FriendsController < ApplicationController
     end
   end
 
+  def correct_user
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friend
